9c184885a6951507cf74b303c558b26379744f7a
Rafed Alwani <rafed.alwani@gmail.com>
2021-10-08 17:10:09 -0400

    finished computer screen layout + the ability to recognize which channel was clicked on.

---
+++ script.js
@@ -0,0 +1,43 @@
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+      //height: '360',
+      width: '100%'//,
+      //videoId: 'M7lc1UVf-VE'
+  });
+}
+const channelList = document.querySelector('.list');
+
+const getChannelsList = async () => {
+  
+  let uri = 'http://localhost:3000/channels';
+  const res = await fetch(uri);
+  const channels = await res.json();
+
+  let template = '';
+  channels.forEach(channel => {
+    template += `
+    <a href = "${channel.youtubeId}">
+    <div class="card" id="${channel.youtubeId}">${channel.name}</div>
+    </a>
+    `;
+  });
+  channelList.innerHTML = template;
+}
+
+const switchChannel = e => {
+  e.preventDefault();
+  if (e.target.classList.contains('card'));
+    console.log(e.target.id);
+}
+
+channelList.addEventListener('click', switchChannel);
+addEventListener ('DOMContentLoaded', () => getChannelsList ());
